b84b4c88befa159160d914dd3e1d93151ee0f3d7
Marcin <kazik1142@gmail.com>
2022-03-21 20:57:42 +0100

    fixed nav mobile

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
 const navMobile = document.querySelector('.nav-mobile')
+const links = document.querySelectorAll('.nav__link')
 const navBtn = document.querySelector('.hamburger')
 const footerYear = document.querySelector('.footer__year')
 
@@ -10,6 +11,12 @@ const handleCurrentYear = () => {
 const handleNav = () => {
 	navBtn.classList.toggle('is-active')
 	navMobile.classList.toggle('nav-mobile--active')
+	links.forEach((item) => {
+		item.addEventListener('click', () => {
+			navBtn.classList.remove('is-active')
+			navMobile.classList.remove('nav-mobile--active')
+		})
+	})
 }
 
 handleCurrentYear()
